15c47ae0dd90fbd68748b6523175cd1e60c20c3b
a.bracykhin <bratsykhin_af@str-sib.ru>
2023-10-25 13:29:43 +0700

    add byte[] createConnectionMess()

--- app/src/main/java/com/example/siuandroidconfigurator/SiuDevice.java
+++ app/src/main/java/com/example/siuandroidconfigurator/SiuDevice.java
@@ -8,10 +8,10 @@ public class SiuDevice {
         CONNECTED,
         DISCONNECTING
     }
-
-    private Connection connection_state;
-    private boolean bus_busy;
-    private int serial_num;
+//------------------
+    private Connection connection_state = Connection.CONNECTED;
+    private boolean bus_busy = false;
+    private int serial_num = 0;
     private static final int[] baud_rates_list = new int[]{9600, 19200,4800,2400}; //поддерживаемые скорости
     private static final int[] parity_list = new int[]{0, 2,1}; // 0 - NONE; 1 - ODD; 2 - EVEN
     private static final int[] stop_bits_list = new int[]{0, 1,2};// 0 - 1stop; 1 - 1.5stop; 2 - 2stop
@@ -31,7 +31,18 @@ public class SiuDevice {
     private final int INDEX_COMMAND_NEG = 1;
     private final int INDEX_SERIALNUMBER_H = 2;
     private final int INDEX_SERIALNUMBER_L = 3;
+    //------------------
+    public SiuDevice(){
 
+    }
+    public boolean connect(){
+        if(this.connection_state == Connection.CONNECTED) return true;
+        else if (this.connection_state == Connection.DISCONNECTING || this.connection_state == Connection.CONNECTING) return false;
+        this.connection_state = Connection.CONNECTING;
+
+
+        return false;
+    }
     private int crc16(int[] buf, int lenth){
         int crc16 = 0xFFFF;
         int ui16 = 0xA001;
@@ -53,5 +64,21 @@ public class SiuDevice {
             lenth -= 1;
         }
         return crc16;
+    }
+    private byte[] createConnectionMess(){
+        int count_bytes = 6; // сообщение состоит из 6 байт
+        byte[] res = new byte[count_bytes];
+        int[] tmp_res = new int[count_bytes];
+        tmp_res[INDEX_COMMAND] = COMMAND_CONNECTION;
+        tmp_res[INDEX_COMMAND_NEG] = 255-COMMAND_CONNECTION;
+        tmp_res[INDEX_SERIALNUMBER_H] = ((this.serial_num)& 0xFF00 >> 8);
+        tmp_res[INDEX_SERIALNUMBER_L] = (this.serial_num & 0x00FF);
+        int tmp_crc = crc16(tmp_res, tmp_res.length);
+        tmp_res[count_bytes-2] = tmp_crc & 0x00FF;
+        tmp_res[count_bytes-1] = (tmp_crc & 0xFF00)>>8;
+        for (int i = 0; i < count_bytes; i++) {
+            res[i] = (byte)(tmp_res[i]);
+        }
+        return res;
     }
 }
